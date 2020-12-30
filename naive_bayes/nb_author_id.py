0eb55a0c86f2d52b24bf5eecdb3b96e6525c2b30
Rahul Dhankhar <cse.2278@gmail.com>
2020-12-30 13:06:12 +0530

    Initial commit

---
+++ naive_bayes/nb_author_id.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+
+import sys
+from time import time
+
+sys.path.append(r"C:\Users\RahulDhankhar\PycharmProjects\UdacityML\ud120-projects\tools")
+from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+#########################################################
+# your code goes here ###
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time:", round(time()-t0, 3), "s")
+prediction = clf.predict(features_test)
+accuracy = accuracy_score(labels_test, prediction)
+print("Model Accuracy:", accuracy)
+
+
+#########################################################
